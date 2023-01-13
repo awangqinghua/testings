9881b7add26380c06a27aad4e647a747b129a41b
wangqinghua <867075698@qq.com>
2023-01-13 16:28:26 +0800

    123.03

--- login_test/lesson.py
+++ login_test/lesson.py
@@ -12,6 +12,6 @@ import requests
 
 def test_login():
     url = 'http://www.baidu.com'
-    data = {"datas": "python自动化"}
+    data = {"datas": "功能测试"}
     res = requests.get(url, data)
     return res
