4cf595ec5746b5381b47ec6b4fc47cb68e5130c1
wangqinghua <867075698@qq.com>
2023-01-13 15:24:36 +0800

    Initial commit

--- login_test/lesson.py
+++ login_test/lesson.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time     : 2023/1/13 15:01
+# @Time     : 2023/1/13 15:24
 # @Author   : wqh
 # @Email    : 867075698@qq.com
-# @File     : test_ing.py
+# @File     : lesson.py
 # @Software : PyCharm
 
-print(5+5)
-print(6+6)
+
+print(1+2)
