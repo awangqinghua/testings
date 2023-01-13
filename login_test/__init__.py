3b850e0c08a26444fcbefce95293badbc4856d87
wangqinghua <867075698@qq.com>
2023-01-13 15:02:31 +0800

    Initial commit

---
+++ login_test/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     : 2023/1/13 15:01
+# @Author   : wqh
+# @Email    : 867075698@qq.com
+# @File     : __init__.py.py
+# @Software : PyCharm
---
+++ login_test/test_ing.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     : 2023/1/13 15:01
+# @Author   : wqh
+# @Email    : 867075698@qq.com
+# @File     : test_ing.py
+# @Software : PyCharm
+
+print(5+5)
+
